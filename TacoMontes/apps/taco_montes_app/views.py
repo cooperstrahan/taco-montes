4c5d22e831ce0c0125304e86aacc57366fdfd82f
cstra <cooperstrahan@gmail.com>
2019-02-26 14:28:33 -0800

    Updated picture sizing

--- TacoMontes/apps/taco_montes_app/views.py
+++ TacoMontes/apps/taco_montes_app/views.py
@@ -5,6 +5,6 @@ def index(request):
     print(os.listdir('./apps/taco_montes_app/static/img'))
     picture = {
         "file_list": os.listdir('./apps/taco_montes_app/static/img'),
-        "key": 'KEY'
+        "key": 'AIzaSyDDgOIrhEpeT1dcsMxvWOUSP25Izv5J1zA'
     }
     return render(request, "index.html", picture)
